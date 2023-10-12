68d8f0d065a03718ecd626076f66727a60421e30
anandkenra <anand2work@gmail.com>
2023-10-12 08:43:04 -0500

    Updated the files in Pulse and Pangea modules with the right test cases

--- geni/pangea/conftest.py
+++ geni/pangea/conftest.py
@@ -1,20 +1,18 @@
 import pytest
-import psycopg2
-from pytest_postgresql import factories
+import requests
 
-# Define a PostgreSQL factory fixture
-postgresql = factories.postgresql('postgresql://username:password@localhost/dbname')
 
-# Define a pytest fixture for database connection
-@pytest.fixture(scope='function')
-def database_connection(postgresql):
-    dsn = postgresql.dsn()
-    conn = psycopg2.connect(
-        host=dsn['host'],
-        port=dsn['port'],
-        user=dsn['user'],
-        password=dsn['password'],
-        database=dsn['database']
-    )
-    yield conn
-    conn.close()
+@pytest.fixture()
+def dev_environment():
+    host = "localhost"
+    port = 5000
+
+    return {
+        "host": host,
+        "port": port,
+    }
+
+
+@pytest.fixture(autouse=True)
+def pulse_url(dev_environment):
+    return f"http://{dev_environment['host']}:{dev_environment['port']}"
--- geni/pulse/conftest.py
+++ geni/pulse/conftest.py
@@ -1,19 +1,20 @@
 import pytest
-import requests
+import psycopg2
+from pytest_postgresql import factories
 
+# Define a PostgreSQL factory fixture
+postgresql = factories.postgresql('postgresql://username:password@localhost/dbname')
 
-@pytest.fixture()
-def dev_environment():
-    host = "localhost"
-    port = 5000
-
-    return {
-        "host": host,
-        "port": port,
-    }
-
-
-@pytest.fixture(autouse=True)
-def pulse_url(dev_environment):
-    return f"http://{dev_environment['host']}:{dev_environment['port']}"
-
+# Define a pytest fixture for database connection
+@pytest.fixture(scope='function')
+def database_connection(postgresql):
+    dsn = postgresql.dsn()
+    conn = psycopg2.connect(
+        host=dsn['host'],
+        port=dsn['port'],
+        user=dsn['user'],
+        password=dsn['password'],
+        database=dsn['database']
+    )
+    yield conn
+    conn.close()
