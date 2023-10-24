88e9f0711c8b65e3815a6d88c0ebf94d5e8f989f
Vijaya-Kenra <vijayap.kota@gmail.com>
2023-10-23 22:07:12 -0500

    small changes

--- genie/pulse/index_companies/test_sectors_subsectors.py
+++ genie/pulse/index_companies/test_sectors_subsectors.py
@@ -1,11 +1,11 @@
 import pytest
 from sqlalchemy import create_engine, text
 
-@pytest.mark.skip
+
 @pytest.mark.TRISQUARE_9
 def test_validate_sector_count(db_connection):
     sql_query = f"SELECT COUNT(DISTINCT SECTOR) FROM SECTORS_SUBSECTORS_MV"
     result = db_connection.execute(text(sql_query))
     row_count = result.scalar()
-    assert row_count == 11 
+    assert row_count == 0
 
